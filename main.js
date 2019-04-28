23a741fbc28d808bbffffa64e28df7697224915e
RooToBot <46084338+RooToBot@users.noreply.github.com>
2019-04-29 01:30:50 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,145 +1,98 @@
-class Clock extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {time: 1500, timeShow: moment(1500000).format('mm:ss'), play: 0, type: "counter"};
-    this.startStop = this.startStop.bind(this);
-
-    this.bIncrement = this.bIncrement.bind(this);
-    this.bDecrement = this.bDecrement.bind(this);
-    this.sIncrement = this.sIncrement.bind(this);
-    this.sDecrement = this.sDecrement.bind(this);
-    
-    this.reset = this.reset.bind(this);
+function display(e) {
+  var string = document.getElementById("display").innerHTML;
+  
+  if (e == "CLS") {
+    document.getElementById("display").innerHTML = "0";
   }
   
-  tick() {
-    if (this.state.time == 0) {
-      if (this.state.type == "counter"){
-        clearInterval(this.interval);
-        this.state.time = document.getElementById("break-length").innerHTML * 60;
-        this.interval = setInterval(() => this.tick(), 1000);
-        document.getElementById("timer-label").innerHTML = "BREAK";
-        document.getElementById("beep").play();
-        this.state.type = "break";
+  else if (e == "EQUALS") {
+    document.getElementById("display").innerHTML = eval(string);
+    return;
+  }
+  
+  else if (string == "0" && (e == "/" || e == "*" || e == "-" || e == "+")) {
+    return;
+  }
+  
+  else if ((string.substr(-1) == "/" || string.substr(-1) == "*" || string.substr(-1) == "-" || string.substr(-1) == "+") && (e == "/" || e == "*" || e == "-" || e == "+")) {    
+    if (e == "/" || e == "*" || e == "+" || e == "-") {
+      if (string.substr(-1) == "/" || string.substr(-1) == "*" || string.substr(-1) == "-" || string.substr(-1) == "+") {
+        string = string.replace(string.substr(-1), e);
+        document.getElementById("display").innerHTML = string;
       }
-      
       else {
-        clearInterval(this.interval);
-        this.state.time = document.getElementById("session-length").innerHTML * 60;
-        this.interval = setInterval(() => this.tick(), 1000);
-        document.getElementById("timer-label").innerHTML = "SESSION";
-        document.getElementById("beep").play();
-        this.state.type = "counter";
+        document.getElementById("display").innerHTML = e;
       }
     }
     
-    this.setState(prevState => ({
-      time: prevState.time - 1,
-      timeShow: moment(prevState.time * 1000).format('mm:ss')
-    }));
-
+    return;
   }
   
-  startStop() {    
-    if (this.state.play == 1) {
-      clearInterval(this.interval);
-      this.state.play = 0;
+  else if (string == "0") {
+    if (e == ".") {
       return;
     }
     
-    else if (this.state.play == 0) {
-      this.interval = setInterval(() => this.tick(), 1000);
-      this.state.play = 1;
+    document.getElementById("display").innerHTML = e;
+    return;
+  }
+  
+  else if (e == ".") {
+    if (string.substr(-1) == ".") {
       return;
     }
+    
+    var list = string.split(".");
+    document.getElementById("XXX").innerHTML = list;
+    /*alert(list + " " + list.length);*/
+    
+    if (list.length > 1 && list[list.length - 1].length < 3) {
+      return;
+    }
+    
+    document.getElementById("display").innerHTML = string + e;
+    return;
   }
   
-  /*componentDidMount() {
-    this.interval = setInterval(() => this.tick(), 1000);
+    else if (e == '.' && (string.substr(-1) == "." || string == 0)) {
+    return;
   }
   
-  componentWillUnmount() {
-    clearInterval(this.interval);
-  }*/
-  
-  bIncrement() {
-    if (document.getElementById("break-length").innerHTML < 60 && this.state.play == 0) {
-      document.getElementById("break-length").innerHTML = parseInt(document.getElementById("break-length").innerHTML, 10) + 1;
-      this.state.time = document.getElementById("session-length").innerHTML * 60;
-    }
-  }
-        
-    bDecrement() {
-      if (document.getElementById("break-length").innerHTML > 1 && this.state.play == 0) {
-        document.getElementById("break-length").innerHTML = document.getElementById("break-length").innerHTML - 1;
-        this.state.time = document.getElementById("session-length").innerHTML * 60;
-      }
-    }
-        
-    sIncrement() {
-      if (document.getElementById("session-length").innerHTML < 60 && this.state.play == 0) {
-        document.getElementById("session-length").innerHTML = parseInt(document.getElementById("session-length").innerHTML, 10) + 1;
-        this.state.time = document.getElementById("session-length").innerHTML * 60;
-      }
-    }
-        
-    sDecrement() {
-      if (document.getElementById("session-length").innerHTML > 1 && this.state.play == 0) {
-        document.getElementById("session-length").innerHTML = document.getElementById("session-length").innerHTML - 1;
-        this.state.time = document.getElementById("session-length").innerHTML * 60;
-      }
-    }
-  
-  reset() {
-    document.getElementById("break-length").innerHTML = 5;
-    document.getElementById("session-length").innerHTML = 25;
-    document.getElementById("time-left").innerHTML = moment(1500 * 1000).format('mm:ss');
-    this.state.play = 0;
-    this.state.time = 1500;
-    document.getElementById("beep").pause();
-    clearInterval(this.interval);
-  }
-  
-  render() {
-    return (
-      <div id="wrapper">
-        <h1>Pomodoro Clock</h1>
-        <div id="controls">
-          <div>
-            <div id="child">
-              <p id="break-label">Break Length</p>
-              
-              <span id="break-increment" onClick={this.bIncrement}>UP</span>
-              <span id="break-length">5</span>
-              <span id="break-decrement" onClick={this.bDecrement}>DOWN</span>
-            </div>
-            
-            <div id="child">
-              <p id="session-label">Session Length</p>
-              
-              <span id="session-increment" onClick={this.sIncrement}>UP</span>
-              <span id="session-length">25</span>
-              <span id="session-decrement" onClick={this.sDecrement}>DOWN</span>
-            </div>
-          </div>
-          <div id="wrapper2">
-            <h4 id="timer-label">SESSION</h4>
-            <h1 id="time-left">{this.state.timeShow}</h1>
-            <span id="start_stop" onClick={this.startStop}>
-              <img src="http://www.clker.com/cliparts/G/2/S/R/a/9/start-stop-btn-on.svg.hi.png">
-              </img>
-            </span>
-            
-            <span id="reset" onClick={this.reset}>
-              <img src="https://image.flaticon.com/icons/svg/954/954000.svg">
-              </img>
-            </span>
-          </div>
-        </div>
-        <audio id="beep" preload="auto" src="https://goo.gl/65cBl1"/>
-      </div>
-    );
+  else {
+    document.getElementById("display").innerHTML = string + e;
+    return;
   }
 }
 
-ReactDOM.render(<Clock />, document.getElementById('App'));
+function Calculator() {
+  return (
+  <div id="wrapper">
+      <div id="display">0</div>
+      
+      <button id="clear" onClick={() => this.display("CLS")}>AC</button>
+      <button id="divide" onClick={() => this.display("/")}>/</button>
+      <button id="multiply" onClick={() => this.display("*")}>X</button>
+      
+      <button id="one" onClick={() => this.display(1)}>1</button>
+      <button id="two" onClick={() => this.display(2)}>2</button>
+      <button id="three" onClick={() => this.display(3)}>3</button>
+      <button id="subtract" onClick={() => this.display("-")}>-</button>
+      
+      <button id="four" onClick={() => this.display(4)}>4</button>
+      <button id="five" onClick={() => this.display(5)}>5</button>
+      <button id="six" onClick={() => this.display(6)}>6</button>
+      <button id="add" onClick={() => this.display("+")}>+</button>
+      
+      <button id="seven" onClick={() => this.display(7)}>7</button>
+      <button id="eight" onClick={() => this.display(8)}>8</button>
+      <button id="nine" onClick={() => this.display(9)}>9</button>
+      
+      <button id="zero" onClick={() => this.display(0)}>0</button>
+      <button id="decimal" onClick={() => this.display(".")}>.</button>
+      <button id="equals" onClick={() => this.display("EQUALS")}>=</button>   
+  </div>
+    
+  );
+}
+
+ReactDOM.render(<Calculator />, document.getElementById('App'));
